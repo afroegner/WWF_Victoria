2c671d04d56ebe6bb316832d86dc1a8e4919bf45
Jessica Corman <jrcorman@asu.edu>
2018-07-24 20:53:25 +0000

    Added code for site information data frame

---
+++ code/DataPrep_SiteInfo.R
@@ -0,0 +1,6 @@
+# Creation of beach site information data file
+
+Sites <- read.csv("Site_Info.csv")
+
+# Need to add info on water source and % primarily collected from water based
+# on house hold survey information
