7ab655639481dba0acbac8a7e968355b0f5d54fc
Jessica Corman <jrcorman@asu.edu>
2018-07-25 16:18:22 +0000

    Create map of all sites with community survey or water quality data and with only relevant sites

--- code/DataPrep_SiteInfo.R
+++ code/DataPrep_SiteInfo.R
@@ -1,6 +1,16 @@
 # Creation of beach site information data file
 
-Sites <- read.csv("Site_Info.csv")
+Communities <- read.csv("/nfs/waterwomenfisheries-data/Site_Info.csv")
+Lake_Sites <- read.csv("/nfs/waterwomenfisheries-data/Site_Info_Lakes.csv")
 
 # Need to add info on water source and % primarily collected from water based
 # on house hold survey information
+
+# Want to only use data from sites with both community and water sampling
+
+levels(Communities$Site)
+levels(Lake_Sites$Site)
+
+locations <- intersect(Communities$Site, Lake_Sites$Site)
+
+GPS_overlaps <- Communities[Communities$Site %in% locations, ]
---
+++ code/Figure_SiteMap.R
@@ -0,0 +1,27 @@
+# Creation of site map
+
+source("code/DataPrep_SiteInfo.R")
+
+library(ggmap)
+# Map of Winam Gulf with all sites
+bbox <- c(left=34,bottom=-0.6,right=34.9,top=0)
+g <- ggmap(get_stamenmap(bbox, zoom=11, maptype="toner-lite"))
+g +  geom_point(data=Lake_Sites,
+                aes(x=Longitude, y=Latitude, color=Location), size=3, alpha=0.95) +
+  geom_point(data=Communities,
+             aes(x=Longitude, y=Latitude), alpha=0.5, color="black") +
+  geom_hline(yintercept = 0, color = "black") +
+  geom_vline(xintercept = 34.9, color="black") +
+  theme_classic(12) 
+
+# Map of Winam Gulf with only relevant sites
+bbox <- c(left=34,bottom=-0.6,right=34.9,top=0)
+g <- ggmap(get_stamenmap(bbox, zoom=11, maptype="toner-lite"))
+g +  geom_point(data=GPS_overlaps,
+                aes(x=Longitude, y=Latitude), size=3, alpha=0.95) +
+  geom_hline(yintercept = 0, color = "black") +
+  geom_vline(xintercept = 34.9, color="black") +
+  theme_classic(12) 
+ggsave("figures/Map_WinamGulf.png", width=5, height=4)
+  
+             
