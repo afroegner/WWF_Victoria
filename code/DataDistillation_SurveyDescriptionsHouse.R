621668182a743b5060fec3d5c3b10aaea4c3fd5f
Jessica Corman <jrcorman@asu.edu>
2018-07-25 20:26:53 +0000

    code for data distillation of surveys

--- code/DataDistillation_SurveyDescriptionsHouse.R
+++ code/DataDistillation_SurveyDescriptionsHouse.R
@@ -1,14 +1,3 @@
 # Generating site descriptors
 
 survey_house <- read.csv("data_out/survey_house_WQ.csv")
-
-# Earliest bloom
-
-house.bloom <- survey_house %>%
-  select(beach.name, earliest.algal.bloom) %>%
-  group_by(beach.name) %>%
-  summarise(
-    eab.min = min(earliest.algal.bloom, na.rm=TRUE),
-    eab.max = max(earliest.algal.bloom, na.rm=TRUE),
-    eab.mean = mean(earliest.algal.bloom, na.rm=TRUE)
-  )
